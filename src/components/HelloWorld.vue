b0d7a9036f7e64b21aef457da7c1c269f98efeb5
Abedur <abed.baust@gmail.com>
2023-03-03 13:53:46 +0600

    [sessionStorage] [Added]

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,43 +2,86 @@
 <template>
   <div>
     <!-- <h1>{{ addClubErrors }}</h1> -->
-    <button @click="increment">Increment</button>
+    <button @click="updateSessionModel">Increment</button>
   </div>
 </template>
 
 <script>
 
+import { ref, onMounted } from 'vue';
+
 export default {
-  components: {
+  setup() {
+    const tabId = ref(window.name);
+    onMounted(() => {
+      // Generate a new ID if one doesn't exist
+      if (!tabId.value) {
+        tabId.value = Math.random().toString(36).substr(2, 9);
+        window.name = tabId.value;
+        let sessionIdsAndModel = [];
+        console.log(sessionIdsAndModel, "SS");
 
-  },
-
-  computed: {
-    addClubErrors() {
-      return this.$store.state.count;
-    },
+        let tabIdAndModel = {
+          sessionId: window.name,
+          model: [],
+        };
+        console.log(tabIdAndModel, "abc");
+        sessionIdsAndModel.push(tabIdAndModel);
+        window.sessionStorage.setItem(
+          "sessionIdsAndModel",
+          JSON.stringify(sessionIdsAndModel)
+        );
+      }
+    });
+    // Remove data from sessionStorage when tab is closed
+    // window.addEventListener("beforeunload", () => {
+    //   let sessionIdsAndModel =
+    //     JSON.parse(window.sessionStorage.getItem("sessionIdsAndModel")) || [];
+    //   sessionIdsAndModel = sessionIdsAndModel.filter(
+    //     (obj) => obj.sessionId !== tabId.value
+    //   );
+    //   window.sessionStorage.setItem(
+    //     "sessionIdsAndModel",
+    //     JSON.stringify(sessionIdsAndModel)
+    //   );
+    // });
   },
   data: function () {
     return {
-      count: 0
+      sessionId: 0,
+      dashboardModel: {
+        name: "abed",
+        Id: 1
+      },
     };
   },
+  mounted() {
+    // window.addEventListener("beforeunload", () => {
+    //   this.clearSession(window.name, "specificPropName");
+    // });
+  },
   methods: {
-    increment() {
-      const payload = {
-  name: "Size1",
-  description: "Size2"
+    updateSessionModel() {
+      // Retrieve the sessionIdsAndModel array from sessionStorage
+      let sessionIdsAndModel =
+        JSON.parse(window.sessionStorage.getItem("sessionIdsAndModel")) || [];
+
+      // Find the index of the object with the specified sessionId
+      let index = sessionIdsAndModel.findIndex(
+        (obj) => obj.sessionId === window.name
+      );
+
+      // Update the model property of the object at the specified index
+      sessionIdsAndModel[index].model = this.dashboardModel;
+
+      // Store the updated sessionIdsAndModel array back in sessionStorage
+      window.sessionStorage.setItem(
+        "sessionIdsAndModel",
+        JSON.stringify(sessionIdsAndModel)
+      );
+    },
+  }
 }
 
-// const payload = {
-//   emailOrPhone: "abed.baust1@gmail.com",
-//   password: "Dummy@123",
-//   rememberMe: true
-// }
-      console.log('aaa',payload);
-      this.$store.dispatch("getToken2",payload);
-    }
-  },
-};
 
 </script>
